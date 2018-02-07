c3ff4d165a3fa2ab7b6b4a9f3156a97419975480
Yanglangfei <yanglangfei@jucaipen.com>
2018-02-07 14:05:12 +0800

    异常捕获

---
+++ ai/src/main/java/com/yf/web/ai/base/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.yf.web.ai.base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+/**
+ * 全局异常处理
+ * @author  ylf
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String noHandleException(Exception ex){
+        log.error("error:["+ex.getMessage()+"]");
+        return "ApiNotFound";
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String requestMissingServletRequest(MissingServletRequestParameterException ex){
+        log.error("error:["+ex.getMessage()+"]");
+        return "ParamNecessary";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public String   error(Exception e){
+        log.error("error:["+e.getMessage()+"]");
+        return "系统异常";
+    }
+
+
+}
