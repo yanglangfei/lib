24a88a020e479a346e46f7b112e283195cf2a27a
Yanglangfei <yanglangfei@jucaipen.com>
2018-02-08 18:17:06 +0800

    update swagger

--- ai/src/main/java/com/yf/web/ai/config/SwaggerConfig.java
+++ ai/src/main/java/com/yf/web/ai/config/SwaggerConfig.java
@@ -3,17 +3,16 @@ package com.yf.web.ai.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -28,15 +27,13 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 // 任意，请稍微规范点
-                .title("v1.1.0")
+                .title("API Docs")
                 // 任意，请稍微规范点
-                .description("app1")
-                // 将“url”换成自己的ip:port
-                .termsOfServiceUrl("http://192.168.1.139/swagger-ui.html")
+                .description("API Docs for Swagger")
+                .termsOfServiceUrl("http://yf.com")
                 // 无所谓（这里是作者的别称）
-                .contact("ylf")
-                .license("lincs")
-                .version("1.1.0")
+                .contact(new Contact("fengerous","http://yc.blog.com","185601452@gmail.com"))
+                .version("1.0.0")
                 .build();
     }
 
